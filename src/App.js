5af8addf3f540ad708a15639a4a81670a1939b43
Sara Khare <sarakhare@Saras-MacBook-Pro.local>
2026-04-09 16:45:15 +0530

    updated booking popup projects

--- src/App.js
+++ src/App.js
@@ -20,25 +20,24 @@ import WasTumdi from "./pages/projects/Tumdi";
 function App() {
   return (
     <Router>
-      <div className="w-full overflow-x-hidden"></div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        
-<Route path="/projects/chikana" element={<WasChikana />} />
-<Route path="/projects/dhamana" element={<WasDhamana />} />
-<Route path="/projects/tumdi" element={<WasTumdi />} />
-<Route path="/emi" element={<EmiCalculator />} />
-<Route path="/emi-calculator" element={<EmiCalculator />} />
-<Route path="/contact" element={<ContactUs />} />
-<Route path="/about" element={<AboutUs />} />
-<Route path="/blogs" element={<Blogs />} />
-        <Route path="/blogs/:id" element={<BlogDetails />} />
+      <div className="w-full overflow-x-hidden">
+        <Navbar />
 
+        <Routes>
+          <Route path="/" element={<Home />} />
 
+          <Route path="/projects/chikana" element={<WasChikana />} />
+          <Route path="/projects/dhamana" element={<WasDhamana />} />
+          <Route path="/projects/tumdi" element={<WasTumdi />} />
 
-      </Routes>
-<div/>
+          <Route path="/emi" element={<EmiCalculator />} />
+          <Route path="/emi-calculator" element={<EmiCalculator />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/:id" element={<BlogDetails />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
